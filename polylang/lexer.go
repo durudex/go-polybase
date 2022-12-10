ac962d6bb247feb3a681d7a939b252fd2facf2e6
V1def <vaniya8871@gmail.com>
2022-12-10 21:50:59 +0100

    Added stateful lexer and updated polylang tests

---
+++ polylang/lexer.go
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2022 V1def
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package polylang
+
+import "github.com/alecthomas/participle/v2/lexer"
+
+var PolylangLexer = lexer.MustSimple([]lexer.SimpleRule{
+	{Name: "Ident", Pattern: `[a-zA-Z_][a-zA-Z0-9_]*`},
+	{Name: "Punct", Pattern: `\[|]|[.,:;={}()?@]`},
+	{Name: "whitespace", Pattern: `\s+`},
+})
