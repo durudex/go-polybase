65563fbbcf8d89556e7348998cc4b5bbc686c46e
V1def <vaniya8871@gmail.com>
2022-12-22 15:22:03 +0100

    Added documentation to the option.go

--- option.go
+++ option.go
@@ -13,35 +13,47 @@ import (
 	"net/url"
 )
 
+// Option interface stores methods for interacting with the specified Polybase record.
 type Option interface {
-	Get(ctx context.Context, v any) error
-	Call(ctx context.Context, method string, args []any, v any) error
+	// Get method sends a request to retrieve all fields of the collection by the specified ID and
+	// decodes the returned value.
+	Get(ctx context.Context, resp any) error
+
+	// Call method calls a function from the Polybase collection scheme with the specified arguments.
+	// To make it easier to pass arguments, you can pass a structure using the ParseInput function.
+	Call(ctx context.Context, fc string, args []any, resp any) error
 }
 
+// option structure implements all methods of the Option interface.
 type option struct {
 	client   Client
 	endpoint string
 }
 
+// NewOption function returns a new record option.
 func NewOption(client Client, endpoint string) Option {
 	return option{client: client, endpoint: endpoint}
 }
 
-func (o option) Get(ctx context.Context, v any) error {
+// Get method sends a request to retrieve all fields of the collection by the specified ID and
+// decodes the returned value.
+func (o option) Get(ctx context.Context, resp any) error {
 	req := &Request{
 		Endpoint: o.endpoint,
 		Method:   "GET",
 	}
 
-	return o.client.MakeRequest(ctx, req, v)
+	return o.client.MakeRequest(ctx, req, resp)
 }
 
-func (o option) Call(ctx context.Context, method string, args []any, v any) error {
+// Call method calls a function from the Polybase collection scheme with the specified arguments.
+// To make it easier to pass arguments, you can pass a structure using the ParseInput function.
+func (o option) Call(ctx context.Context, fc string, args []any, resp any) error {
 	req := &Request{
-		Endpoint: o.endpoint + fmt.Sprintf("/call/%s", url.QueryEscape(method)),
+		Endpoint: o.endpoint + fmt.Sprintf("/call/%s", url.QueryEscape(fc)),
 		Method:   "POST",
 		Body:     Body{Args: args},
 	}
 
-	return o.client.MakeRequest(ctx, req, v)
+	return o.client.MakeRequest(ctx, req, resp)
 }
